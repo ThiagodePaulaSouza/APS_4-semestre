38c3c1678271f1f6a9128c01197151c7e7205e22
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2021-09-14 22:50:13 -0300

    first commit

---
+++ EstacaoMetereologica/src/estacaometereologica/EstacaoMetereologica.java
@@ -0,0 +1,9 @@
+package estacaometereologica;
+
+public class EstacaoMetereologica
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello mundo");
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# APS_4-semestre
