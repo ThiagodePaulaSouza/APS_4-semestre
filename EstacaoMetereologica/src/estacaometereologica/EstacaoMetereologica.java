bea5fb2342f787b722023ac19936e55f61e716fc
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2021-09-14 23:42:04 -0300

    teste de branch

--- EstacaoMetereologica/src/estacaometereologica/EstacaoMetereologica.java
+++ EstacaoMetereologica/src/estacaometereologica/EstacaoMetereologica.java
@@ -4,6 +4,6 @@ public class EstacaoMetereologica
 {
     public static void main(String[] args)
     {
-        System.out.println("hello mundo");
+        System.out.println("hello world");
     }
 }
