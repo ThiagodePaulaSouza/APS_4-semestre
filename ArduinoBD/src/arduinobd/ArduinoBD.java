b445e8f91a02888329d2e7184b556d9af0b1ce3f
ThiagodePaulaSouza <thiagodepaulasouza@gmail.com>
2021-11-11 02:29:22 -0300

    é

--- ArduinoBD/src/arduinobd/ArduinoBD.java
+++ ArduinoBD/src/arduinobd/ArduinoBD.java
@@ -1,27 +1,22 @@
 package arduinobd;
-
 import Apresentacao.frmPrincipal;
 import Controle.Controle;
 import modelo.Serial;
-
 public class ArduinoBD
 {
-
     public static void main(String[] args)
     {
         Serial serial = new Serial();
         if (serial.iniciaSerial())
         {
-            //chamar controle para cadastrar as info
+            
             frmPrincipal frmp = new frmPrincipal(null, true);
             frmp.setVisible(true);
-
+            
             Controle controle = new Controle();
-            //colocar timer caso o de cada classe pareça estar pesado
             controle.cadastrarPrecipitacao();
             controle.cadastrarTemperatura();
             controle.cadastrarUmidade();
-
         }
         serial.close();
         System.exit(0);
